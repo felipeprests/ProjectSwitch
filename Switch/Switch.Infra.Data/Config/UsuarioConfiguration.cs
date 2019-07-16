84a493f3888219c8a470c87f2fd23bec3843a8ae
felipeprests <felipe.adriano@univille.br>
2019-07-16 20:19:05 -0300

    Enviando projeto completo

---
+++ Switch/Switch.Infra.Data/Config/UsuarioConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Switch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Config
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(u => u.ID);
+            builder.Property(u => u.Nome).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.SobreNome).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.Email).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.Senha).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.Sexo).IsRequired();
+            builder.Property(u => u.DataNascimento).IsRequired();
+            builder.Property(u => u.UrlFoto).IsRequired();
+        }
+    }
+}
---
+++ Switch/Switch.Infra.Data/Migrations/20190601231109_AdicionandoUsuarioConfiguration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class AdicionandoUsuarioConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlFoto",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SobreNome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlFoto",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SobreNome",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+        }
+    }
+}
